31268e9edb36c6d95567c4f3bb8f7a9f1572e933
Jyoti <jyoti.nambiar@medieinstitutet.se>
2021-06-16 10:58:20 +0200

    user-bookings link changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -85,7 +85,7 @@ console.log("This is the uploaded image",e.target.files[0]);
         
         //load data in user-bookings in strapi
         
-        axios.post("http://localhost:1337/user-bookings", {
+        axios.post("https://pik-span-strapi.herokuapp.com/user-bookings", {
             name: username,
             date: formValues.date,
             time: formValues.time,
